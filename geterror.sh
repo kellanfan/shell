61503e07016b2d8eb18cccbac84de530a9a4f741
kellanfan <icyfk1989@163.com>
2017-02-23 18:19:01 +0800

    geterror.sh

--- geterror.sh
+++ geterror.sh
@@ -11,63 +11,104 @@
 ####################################################
 
 SHELL_NAME=`basename $0`
-SHELL_DIR="/usr/local/sbin"
 SHELL_LOG="/var/log/${SHELL_NAME}.log"
 LOCK_FILE="/tmp/${SHELL_NAME}.lock"
+NOTIFY_DIR="/notifier"
+FILES=`find /notifier -path "/notifier/sort" -prune -o -type f -mtime -1 -not -name "*.sign"`
+
 
 shell_log() {
      LOG_INFO=$1
      echo "$(date +%F) $(date +%R:%S) : ${SHELL_NAME} : ${LOG_INFO}" >> ${SHELL_LOG}
 }
-usage() {
-    echo "Usage: ${SHELL_NAME}  "
-}
 
 shell_lock() {
      touch ${LOCK_FILE}
 }
+
 shell_unlock() {
      rm -f ${LOCK_FILE}
 }
 
 del_timeoutfile() {
-	find /root/ -name "result*" -mtime +1 -print | xargs rm -rf
-	find /notifier -mtime +1 -type f |xargs rm -rf
+	find /notifier -mtime +1 -type f -print |xargs rm -rf &> /dev/null
 	if [ $? == 0 ];then
 		shell_log "clean up done"
 	else
 		shell_log "clean fail"
 	fi
+	rm /notifier/sort/*
 }
 
-sort_log() {
+
+checkfile() {
+	FILES=`find /notifier -path "/notifier/sort" -prune -o -type f -mtime -1 -not -name "*.sign"`
+	declare -a array
+	for file in $FILES; do
+		NUM=`nl $file |egrep '<h3>'|wc -l`
+		LIST=`nl $file |egrep '<h3>'|awk '{print $1}'|xargs echo`
+		re_file=`basename ${file}`
+		if [ "${NUM}" -gt 1 ]; then
+			i=0
+			for list in $LIST;do
+				array[$i]=$list
+				let "i += 1"
+			done
+			j=0
+			while (( j < $NUM )); do
+				sed -n "${array[$j]},${array[$j+1]}p" $file > $NOTIFY_DIR/sort/$re_file-$j
+				let "j += 1"
+			done
+		else
+			cp $file $NOTIFY_DIR/sort/
+		fi
+	
+	done
+}
+
+awklog() {
 	DATE=`date +%F`
-	FILES=`find /notifier -type f -mtime -1`
 	HOST_NAME=''
 	KEY=''
-	for file in $FILES
-	do
-		HOST_NAME=`basename $file | awk -F"_" '{print $3}'`
-		KEY=`awk '/\<h3\>/{print $2}' $file|awk -F"[" '{print $1}'`
-		for key in $KEY
-		do
-			if [[ $key = kern.log ]];then
-				awk '/\<p\>/{for(i=7;i<=NF;i++) printf $i" ";printf "\n"}' $file |sort|uniq >> /root/result-$HOST_NAME-$DATE
-			elif [[ $key =~ wf ]];then
-				awk '/\<p\>/{for(i=7;i<=NF;i++) printf $i" ";printf "\n"}' $file |sort|uniq >> /root/result-$HOST_NAME-$DATE
-			elif [[ $key = supervisord.log ]];then
-				awk '/\<p\>/{for(i=3;i<=NF;i++) printf $i" ";printf "\n"}' $file |sort|uniq >> /root/result-$HOST_NAME-$DATE
-			else
-				awk '/\<p\>/{print $0}' $file|sort| uniq >> /root/result-$HOST_NAME-$DATE
-			fi
-		done		
-	done
-	if [ -f /root/result-$HOST_NAME-$DATE ];then
-		cat /root/result-$HOST_NAME-$DATE | sort |uniq >> /root/result-$DATE
+	if [[ -f ${LOCK_FILE} ]];then
+		AWK_FILES=`find $NOTIFY_DIR/sort -type f`	
+		for awk_file in $AWK_FILES; do
+			HOST_NAME=`basename ${awk_file} | awk -F"_" '{print $3}'`
+			KEY=`awk '/\<h3\>/{print $2}' $awk_file|awk -F"[" '{print $1}'`
+			for key in $KEY
+			do
+				if [[ $key = kern.log ]];then
+					awk '/\<p\>/{for(i=7;i<=NF;i++) printf $i" ";printf "\n"}' $awk_file |sort|uniq >> /root/result-$HOST_NAME-$DATE
+				elif [[ $key =~ wf ]];then
+					awk '/\<p\>/{for(i=5;i<=NF;i++) printf $i" ";printf "\n"}' $awk_file |sort|uniq >> /root/result-$HOST_NAME-$DATE
+				elif [[ $key = supervisord.log ]];then
+					awk '/\<p\>/{for(i=3;i<=NF;i++) printf $i" ";printf "\n"}' $awk_file |sort|uniq >> /root/result-$HOST_NAME-$DATE
+				else
+					awk '/\<p\>/{print $0}' $awk_file|sort| uniq >> /root/result-$HOST_NAME-$DATE
+				fi
+			done
+		done
 	fi
 }
-#Subsequent finishing
+
+makeup()
+{
+	FILE_RESULT=`ls /root/result-*`
+	DATE=`date +%F`
+	for i in $FILE_RESULT
+	do
+		cat $i | sort >> /root/$DATE-result-tmp
+	done
+	cat /root/$DATE-result-tmp |sort | uniq > /root/$DATE-result
+	rm /root/$DATE-result-tmp
+	rm /root/result-*
+}
 shell_lock
 del_timeoutfile
-sort_log
+if [ ! -d $NOTIFY_DIR/sort ];then
+	mkdir $NOTIFY_DIR/sort
+fi
+checkfile
+awklog
+makeup
 shell_unlock
