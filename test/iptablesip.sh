0a2fb8aed8fe1e8f84a10c599e599d948f9c2952
kellanfan <icyfk1989@163.com>
2017-06-18 19:22:25 +0800

    shili

---
+++ test/iptablesip.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+DENYIPLIST="/usr/local/sbin/denyiplist"
+
+IPLIST=`grep "Invalid user" /var/log/auth.log |awk '{print $10}'|sort|uniq`
+
+for ip in $IPLIST; do
+    grep $ip $DENYIPLIST > /dev/null
+    if [ $? -ne 0 ]; then
+        iptables -A INPUT -s $ip -p tcp --dport 22 -j DROP
+        echo $ip >> $DENYIPLIST
+    fi
+done
