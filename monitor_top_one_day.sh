6c8ecbae8e27aa0e7af756fef34bb010548dff6f
kellanfan <icyfk1989@163.com>
2018-08-17 11:19:00 +0800

    Add information

--- monitor_top_one_day.sh
+++ monitor_top_one_day.sh
@@ -2,9 +2,12 @@
 #######################################################################
 #Author: kellanfan
 #Created Time : Fri 14 Jul 2017 09:20:39 AM CST
-#File Name: moniteo_top_one_day.sh
-#Description:
+#File Name: monitor_top_one_day.sh
+#Description: Monitor top information for a day to analyze process status.
 #######################################################################
+
+#You need to add a planning task.link this:
+#0 0 * * * bash monitor_top_one_day.sh &
 
 for i in {1..24}; do
     datetime=`date +'%F_%R'`
