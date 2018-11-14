5da24d16d7d11a200aa58d484b0d73a82bedcc75
kellanfan <icyfk1989@163.com>
2018-11-14 14:17:15 +0800

    加上说明

--- clean-docker.sh
+++ clean-docker.sh
@@ -6,14 +6,21 @@
 #Description:
 #######################################################################
 
+echo "清除容器..."
 cn_list=`docker ps -qa`
-
 for i in $cn_list;do
     docker rm $i
 done
 
-#清除dangling image
+echo "清除dangling image..."
 dl_image=`docker images -q -f dangling=true`
 for j in $dl_image;do
     docker rmi $j
 done
+
+echo "清除虚悬volume..."
+dl_volume=`docker volume ls -qf dangling=true`
+for g in $dl_volume;do
+    docker volume rm $g
+done
+echo "Done."
